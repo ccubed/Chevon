682df1e1b483d86e2a884159904287e565fefc8c
ccubed.techno <Ccubed.techno@gmail.com>
2015-07-10 23:05:15 -0400

    Initial
    Initial upload

---
+++ Chevon/API/VAPI.vb
@@ -0,0 +1,21 @@
+﻿Public Class VAPI
+
+    Public Legacy As Boolean
+    Private API_Token As APITKN
+
+
+
+End Class
+
+Public Class APITKN
+
+    Private AccessToken As String
+    Private TKNType As String
+    Private Expiration As Integer
+    Private User As String
+    Private Issued As Date
+    Private Expires As Date
+
+
+
+End Class
